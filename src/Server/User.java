b3dd6fdbf5f85e940d0f9c16981ecbf216852810
RefundGarbage <stevenxu1202@yahoo.com>
2019-06-15 16:34:27 -0700

    finished UI and cleared bugs It should be good to go Also, if you want to make the UI look beautiful, just go to ChatFrame.form, do not change any names of the components. Except for that, play around with the UI and discuss it with Dorothy My job is done here.

--- src/Server/User.java
+++ src/Server/User.java
@@ -6,62 +6,64 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+//manage User info
 class User{
-    private PrintWriter w;
-    private Handler h;
-    private Server server;
+	private PrintWriter w;
+	private Handler h;
+	private Server server;
 	private Socket client;
 
 	public boolean isLogin;
-    private final String username;
-    private final String password;
-    private final String nickname;
+	private final String username;
+	private final String password;
+	private final String nickname;
 
-    User(String u, String p, String n){
-        username = u;
-        password = p;
-        nickname = n;
-        isLogin = false;
-    }
+	User(String u, String p, String n){
+		username = u;
+		password = p;
+		nickname = n;
+		isLogin = false;
+	}
 
-    public String transformMsg(String msg){
-        return nickname + ": " + msg;
-    }
+	public String transformMsg(String msg){
+		return nickname + ": " + msg;
+	}
 
-    public void sendToUser(String msg){
-        w.println(msg);
-        w.flush();
-    }
+	public void sendToUser(String msg){
+		w.println(msg);
+		w.flush();
+	}
 
 	public void login(Socket client, PrintWriter w, BufferedReader r, Server server) {
-        isLogin = true;
+		isLogin = true;
 		this.client = client;
-        this.w = w;
-        this.server = server;
-        h = new Handler(r, this, this.server);
-        h.start();
+		this.w = w;
+		this.server = server;
+		h = new Handler(r, this, this.server);
+		h.start();
 		server.sendAll("User Login: " + nickname);
 		System.out.println("User Login: " + nickname);
-    }
+	}
 
-    public void logOut() throws Exception{
+
+	public void logOut() throws Exception{
 		isLogin = false;
-        h.close();
-        w.close();
+		h.close();
+		w.close();
 		client.close();
 		server.sendAll("User Logout: " + nickname);
 		System.out.println("User Logout: " + nickname);
-    }
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public String getNickname() {
-        return nickname;
-    }
+	public String getNickname() {
+		return nickname;
+	}
 }
